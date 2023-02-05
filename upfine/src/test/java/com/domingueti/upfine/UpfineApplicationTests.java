b81a7207adbe4d3421ba26476b14716ec938c2f9
Daniel Pires Domingueti <danieldomingueti@hotmail.com>
2023-02-05 11:28:37 -0300

    feat: maven + spring versions

---
+++ upfine/src/test/java/com/domingueti/upfine/UpfineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domingueti.upfine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpfineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
