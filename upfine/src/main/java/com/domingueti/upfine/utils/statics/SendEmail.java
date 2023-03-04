2ea4316bd3c0adccaab80163ed1f7c36b5e950c6
DanielDomingueti <danieldomingueti@hotmail.com>
2023-03-04 18:53:19 -0300

    fix: refactoring SendEmail and ExtractCsvLines

--- upfine/src/main/java/com/domingueti/upfine/utils/statics/SendEmail.java
+++ upfine/src/main/java/com/domingueti/upfine/utils/statics/SendEmail.java
@@ -10,38 +10,17 @@ import javax.mail.internet.MimeMultipart;
 import java.util.Properties;
 
 import static javax.mail.Message.RecipientType.TO;
+import static javax.mail.Transport.send;
 import static javax.mail.internet.InternetAddress.parse;
 
 @NoArgsConstructor
 public class SendEmail {
 
     public void execute(String from, String to, String subject, String text) throws MessagingException {
-
        final Session session = Session.getInstance(createSmptProperties(), createAuthenticationWithEmailAndAppPassword());
-       Message message = new MimeMessage(session);
-       message.setFrom(new InternetAddress(from));
-       message.setRecipients(TO, parse(to));
-       message.setSubject(subject);
-
-       MimeBodyPart mimeBodyPart = new MimeBodyPart();
-       mimeBodyPart.setContent(text, "text/html; charset=UTF-8");
-
-       Multipart multipart = new MimeMultipart();
-       multipart.addBodyPart(mimeBodyPart);
-
-       message.setContent(multipart);
-
-       Transport.send(message);
+       Message message = createMessage(session, from, to, subject, text);
+       send(message);
    }
-
-    private Authenticator createAuthenticationWithEmailAndAppPassword() {
-       return  new Authenticator() {
-           @Override
-           protected PasswordAuthentication getPasswordAuthentication() {
-               return new PasswordAuthentication("danielbaudocla@gmail.com", System.getenv("GMAIL_APP_PASSWORD"));
-           }
-       };
-    }
 
     private Properties createSmptProperties() {
         Properties properties = new Properties();
@@ -53,6 +32,40 @@ public class SendEmail {
         properties.put("mail.smtp.ssl.protocols", "TLSv1.2");
         properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
         return properties;
-   }
+    }
+
+    private Authenticator createAuthenticationWithEmailAndAppPassword() {
+        return  new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("danielbaudocla@gmail.com", System.getenv("GMAIL_APP_PASSWORD"));
+            }
+        };
+    }
+
+    private Message createMessage(Session session, String from, String to, String subject, String text) {
+        Message message = new MimeMessage(session);
+        try {
+            message.setFrom(new InternetAddress(from));
+            message.setRecipients(TO, parse(to));
+            message.setSubject(subject);
+            message.setContent(createMultipart(text));
+            return message;
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Multipart createMultipart(String text) {
+        try {
+            MimeBodyPart mimeBodyPart = new MimeBodyPart();
+            mimeBodyPart.setContent(text, "text/html; charset=UTF-8");
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(mimeBodyPart);
+            return multipart;
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
